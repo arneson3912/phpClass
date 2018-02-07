e59dc0a3ad8d9259447f05994f5a1b842efdaa13
arneson3912 <36243235+arneson3912@users.noreply.github.com>
2018-02-07 15:08:50 -0600

    New Homepage

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset ="utf=8">
+    <title>Jeremy's Homepage</title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
+</head>
+<body>
+<header>
+    <h1>Jeremy's Website</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="/">HomePage</a></li>
+        <li><a href="#">Loop</a></li>
+        <li><a href="#">Count Down</a></li>
+    </ul>
+</nav>
+<main>
+    <img src="images/jeremy.jpg" alt="Picture of Jeremy">
+    <p>
+        I have studied the ancient pagan faiths that came before this more recent obsession with a single, divine creator. They seem to have focused more on the fundamental forces at play in the world around us and less on arbitrary moral rules...
+        The sun rises in the morning and sets in the evening. The tides ebb and flow. Grass grows, withers, dies, and then in time, emerges from the ground once more. The air turns warm then cools and back again. Some hidden energy keeps us fitted to the ground and pulls us back when we attempt to leave it.
+        Each of these movements was represented before by a god or goddess. Each force given face, but recognized as something distinct and powerful. Which is not to say there were not connections between these forces – a pantheon of individual spirits – of rules. Invisible hands guiding the progress of the world around us.
+        And so here there was an attempt to categorize, study, explain, and understand the way things work – even if it was flawed. But no more. Now we are asked to succumb to a far more simplified explanation. How naive to believe there might be a single answer to every question. Every mystery. That there exists a lone divine light which rules over all. They say it is a light that brings truth and love. I say it is a light that blinds us – and forces us to stumble about in ignorance.
+        I long for the day when men turn away from invisible monsters and once more embrace a more rational view of the world. But these new religions are so convenient – and promise such terrible punishment should one reject them – I worry that fear shall keep us stuck to what is surely the greatest lie ever told.
+        -Altair
+    </p>
+</main>
+<footer>&copy; 2020 JeremyArneson.com</footer>
+</body>
+
+</html>
